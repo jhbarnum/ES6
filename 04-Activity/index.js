eab87949f5e940c69ce578285faf7636dda12010
Jared <Jared@Littles-MBP.fios-router.home>
2018-03-15 15:08:12 -0400

    4done

--- 04-Activity/index.js
+++ 04-Activity/index.js
@@ -1,10 +1,10 @@
 // 1. const can be used for values which we will not reassign
 //const age = 25;
 
-let age = 25;
+//let age = 25;
 //TypeError: Assignment to constant variable.
-age++;
-console.log(age);
+// age++;
+// console.log(age);
 // const name = "";
 
 // TypeError: Assignment to constant variable.
@@ -15,21 +15,23 @@ console.log(age);
 // 2. const doesn't mean `constant value`, instead means `constant reference`
 
 // Unlike primitive data types, objects and arrays are passed by reference, rather than passed by value
-// const beatles = ["John", "Paul", "Ringo"];
-// beatles.push("George");
+const beatles = ["John", "Paul", "Ringo"];
+beatles.push("George");
 
-// // This works because by updating an array's contents, we aren't changing the reference to the underlying array
-// console.log(beatles); // Prints `["John", "Paul", "Ringo", "George"]`
+// This works because by updating an array's contents, we aren't changing the reference to the underlying array
+console.log(beatles); // Prints `["John", "Paul", "Ringo", "George"]`
 
-// console.log(beatles);
+console.log(beatles);
 
-// const person = { name: "Brianna", age: 11 };
+const person = { name: "Brianna", age: 11 };
 // person.age++;
 // person.favoriteMovie = "Spider-Man";
 // person.name = "Carla";
 
 // console.log(person); // Prints `{ name: 'Carla', age: 12, favoriteMovie: 'Spider-Man' }`
+//person = { name: "jon", age: 11 }; Breaks
 
+console.log(person); 
 // // --------------------------------------------------------------------------
 
 // // 3. While we can MODIFY arrays and objects that are using `const`, we can't reassign them
@@ -53,10 +55,13 @@ console.log(item);
 //   price: "$101.43"
 // };
 
-// const ninjaTurtles = [];
+const ninjaTurtles = [];
 
-// // The same rules apply to arrays, we can MODIFY them, but not completely reassign them
+// The same rules apply to arrays, we can MODIFY them, but not completely reassign them
 
-// // TypeError: Assignment to constant variable.
-// // ninjaTurtles = ["Michaelangelo", "Leonardo", "Raphael", "Donatello"];
+// TypeError: Assignment to constant variable.
+//ninjaTurtles = ["Michaelangelo", "Leonardo", "Raphael", "Donatello"];
 
+ninjaTurtles.push("Michaelangelo", "Leonardo", "Raphael", "Donatello", "steve", "Tony");
+
+console.log(ninjaTurtles)
